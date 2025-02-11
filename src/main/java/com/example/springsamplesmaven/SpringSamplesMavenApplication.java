0cce63d0241535e7333cb1557390d0a9d5d49d52
sungchul.shin <sungchul.shin@intelliantech.com>
2025-02-11 10:22:10 +0900

    Initial commit

---
+++ src/main/java/com/example/springsamplesmaven/SpringSamplesMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsamplesmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSamplesMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSamplesMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsamplesmaven/SpringSamplesMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsamplesmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSamplesMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
